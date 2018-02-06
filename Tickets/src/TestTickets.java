a13f185c5f658a447f3d3a61f1f61ab35ca5ac3c
Tuan Le <tuan.le94.tl@gmail.com>
2018-02-06 17:00:55 +0000

    Test#1 complete

--- Tickets/src/TestTickets.java
+++ Tickets/src/TestTickets.java
@@ -3,10 +3,32 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestTickets {
-
+Ticket ticket = new Ticket(8,6,6,4);
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	public void checkStandardTicketPrices() {
+		int expectedPrice = 8;
+		int standardT = ticket.getStandard();
+		assertEquals(expectedPrice, standardT);
+		
+	}
+	@Test
+	public void checkOapTicketPrices() {
+		int expectedPrice = 6;
+		int oapT = ticket.getOap();
+		assertEquals(expectedPrice, oapT);
+	}
+	@Test
+	public void checkStudentTicketPrices() {
+		int expectedPrice = 6;
+		int studentT = ticket.getStudent();
+		assertEquals(expectedPrice, studentT);
+	}
+	@Test
+	public void checkChildTicketPrices() {
+		int expectedPrice = 4;
+		int childT = ticket.getChild();
+		assertEquals(expectedPrice, childT);
 	}
 
 }
--- Tickets/src/Ticket.java
+++ Tickets/src/Ticket.java
@@ -1,5 +1,42 @@
 
 public class Ticket {
+	private int standard = 8;
+	private int oap = 6;
+	private int student = 6;
+	private int child = 4;
+	
+	public Ticket(int standard, int oap, int student, int child) {
+		super();
+		this.standard = standard;
+		this.oap = oap;
+		this.student = student;
+		this.child = child;
+	}
+	public int getStandard() {
+		return standard;
+	}
+	public void setStandard(int standard) {
+		this.standard = standard;
+	}
+	public int getOap() {
+		return oap;
+	}
+	public void setOap(int oap) {
+		this.oap = oap;
+	}
+	public int getStudent() {
+		return student;
+	}
+	public void setStudent(int student) {
+		this.student = student;
+	}
+	public int getChild() {
+		return child;
+	}
+	public void setChild(int child) {
+		this.child = child;
+	}
 	
 	
 }
+
